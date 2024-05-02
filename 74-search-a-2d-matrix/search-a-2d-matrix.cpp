1afe5ea9245f857efe70a6862a6b442967aabf7f
Shubham Vashishth <43934320+ShubhamVashishth5@users.noreply.github.com>
2024-05-02 23:15:58 +0530

    Time: 4 ms (33.29%) | Memory: 11.9 MB (86.76%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int rows= matrix.size();
+        int column= matrix[0].size();
+        int count= rows*column;
+        int start= 0;
+        int end= count;
+        int mid= start+ (end-start)/2;
+        //std::cout<<rows<<column<<start<<mid<<end<<getAtIndex(mid, column, matrix);
+
+        while(start<end){
+            int curr= getAtIndex(mid, column, matrix);
+            std::cout<<mid;
+            if(curr>target){
+                end= mid;
+                mid= start+ (end-start)/2;;
+            }
+            else if(curr<target){
+                start=mid+1;
+                mid= start+ (end-start)/2;
+            }
+            else if(curr==target) return true;
+
+        }
+        return false;
+    }
+
+    int getAtIndex(int index, int col, vector<vector<int>>& matrix){
+        return matrix[index/col][index%col];
+    }
+
+};
