342f697d9d754bb395a832ab3bb8408f546da669
Shubham Vashishth <43934320+ShubhamVashishth5@users.noreply.github.com>
2024-05-06 03:26:34 +0530

    Time: 44 ms (8.13%) | Memory: 22.1 MB (15.35%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -5,8 +5,12 @@ public:
         int low = 1,  high= piles.back();
         int mid=low + (high-low)/2;
         while(low<high){
-            
-            if (getTime(piles, mid)>h){
+            int sum =0;
+        for(int i=0;i<piles.size();i++){
+            sum+= piles[i]/mid + (piles[i]%mid != 0);
+        }
+
+            if (sum>h){
                 low=mid+1;
             }
             else{
