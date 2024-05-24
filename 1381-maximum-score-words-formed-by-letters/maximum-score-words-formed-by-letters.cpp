d5b61abb1a499ef278c52863454974cfeb0dccb3
Shubham Vashishth <43934320+ShubhamVashishth5@users.noreply.github.com>
2024-05-24 18:37:58 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (94.37%) - LeetSync

---
+++ 1381-maximum-score-words-formed-by-letters/maximum-score-words-formed-by-letters.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int count[26];
+    int maxScoreWords(vector<string>& words, vector<char>& letters,
+                      vector<int>& score) {
+        vector<int> lettersCount(26, 0);
+
+        for (int i = 0; i < letters.size(); i++) {
+            lettersCount[letters[i] - 'a']++;
+        }
+        int current = 0, maxx = 0, index = 0;
+        maxScore(words, current, score, lettersCount, maxx, index);
+        return maxx;
+    }
+
+    void maxScore(vector<string>& words, int& current, vector<int>& score,
+                  vector<int>& letters, int& maxx, int index) {
+
+        maxx = max(maxx, current);
+        for (int i = index; i < words.size(); i++) {
+            bool shouldInsert = true;
+            
+            for (char s : words[i]) {
+                letters[s - 'a']--;
+                current += score[s - 'a'];
+            }
+            for (int i = 0; i < 26; i++) {
+                if (letters[i] < 0)
+                    shouldInsert = false;
+            }
+            if (shouldInsert)
+                maxScore(words, current, score, letters, maxx, i + 1);
+
+            for (char s : words[i]) {
+                letters[s - 'a']++;
+                current -= score[s - 'a'];
+            }
+        }
+    }
+};
